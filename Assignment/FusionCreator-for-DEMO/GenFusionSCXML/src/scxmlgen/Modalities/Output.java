37ca56b919d89898162c1ba8203af16b27bca0f0
jcoelh0 <jcoelho@ua.pt>
2020-01-16 17:35:02 +0000

    presentation added and clean

---
+++ Assignment/FusionCreator-for-DEMO/GenFusionSCXML/src/scxmlgen/Modalities/Output.java
@@ -0,0 +1,39 @@
+package scxmlgen.Modalities;
+
+import scxmlgen.interfaces.IOutput;
+
+
+
+public enum Output implements IOutput{
+    
+	AVANCAR("[action][AvancarL]"),
+	RECUAR("[action][RecuarR]"),
+	ESVAZIARC("[action][EsvaziarC]"),
+	VERC("[action][VerC]"),
+	MCDONALDS("[restaurant][mcdonalds]"),
+	MCDONALDS_UNIVERSIDADE("[restaurant][mcdonalds][place][UNIVERSIDADE]"),
+	MONTADITOS("[restaurant][montaditos]"),
+	SIM("[action][SIM]"),
+	NAO("[action][NAO]"),
+	scrollUpDevagar("[action][scrollUpDevagar]"),
+	scrollUpRapido("[action][scrollUpRapido]"),
+	scrollDownRapido("[action][scrollDownRapido]"),
+	scrollDownDevagar("[action][scrollDownDevagar]"),
+	STOPSCROLL("[action][StopS]"),
+	VERCARRINHO("[action][verC]");    
+    
+    
+    private String event;
+
+    Output(String m) {
+        event=m;
+    }
+    
+    public String getEvent(){
+        return this.toString();
+    }
+
+    public String getEventName(){
+        return event;
+    }
+}
